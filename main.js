041c5bc33e71461b1c3a90fee638f7668c3f1945
Fedot2304 <100525980+Fedot2304@users.noreply.github.com>
2022-03-28 12:26:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,8 @@ $('.slider').slick({
     prevArrow: '<button class="prev"><img src="img/arr1.png"></button>',
     nextArrow: '<button class="next"><img src="img/arr2.png"></button>'
   });
+
+$('.header_burger').click(function(event){
+  $('.header_burger, .header_menu').toggleClass('active');
+  $('body').toggleClass('lock');
+})
