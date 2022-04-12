62f9f6f9f63a4a026d303a240f539243c91d8a22
Fedot2304 <100525980+Fedot2304@users.noreply.github.com>
2022-04-12 18:00:31 +0700

    Add files via upload

--- bg.js
+++ bg.js
@@ -3,27 +3,3 @@ $('.header_burger').click(function(event){
     $('body').toggleClass('lock');
 })
 
-let tabNavItem = document.querySelectorAll('.tabs_item');
-let tabContentItem = document.querySelectorAll('.tabs_block');
-
-tabNavItem.forEach(function(elem){
-    elem.addEventListener('click', activeTab);
-})
-function activeTab(){
-    tabNavItem.forEach(function(elem){
-        elem.classList.remove('active');
-    })
-    this.classList.add('active');
-    let tabName = this.getAttribute('data-tab');
-    activeTabContent(tabName);
-}
-
-function activeTabContent(tabName){
-    tabContentItem.forEach(function(item){
-        if (item.classList.contains(tabName)){
-            item.classList.add('active');
-        } else {
-            item.classList.remove('active')
-        }
-    })
-}
